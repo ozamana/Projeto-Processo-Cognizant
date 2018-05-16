5c90a9074d24605cba7c996f66775f526509fe2e
ozamana <zamana.junior@gmail.com>
2018-05-15 22:45:29 -0300

    Update PaginaInicial.java

--- src/main/java/pageobjects/PaginaInicial.java
+++ src/main/java/pageobjects/PaginaInicial.java
@@ -1,23 +1,16 @@
-package pageobjects;
+package pageobjetcs;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class PaginaInicial extends Page {
-
-	public PaginaInicial(WebDriver driver) {
-		super(driver);
-	}
-
-	public String getUrlIndex() {
-		return driver.getCurrentUrl();
+	
+	public PaginaInicial(WebDriver driver){
+		super (driver);	
 	}
 	
-	public WebElement getSelecionaEstado(String estado) {
-		return getElement(".//li[a = '" + estado + "']//a");
+	public String getURL(){
+		return driver.getCurrentUrl();
+		
 	}
 
-	public void clickSelecionaEstado(WebElement elemento) {
-		elemento.click();
-	}
 }

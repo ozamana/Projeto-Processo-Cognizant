247978493d9c08b015c20847a972303af268c0b5
ozamana <zamana.junior@gmail.com>
2018-05-15 22:43:06 -0300

    Update BuscandoProdutoFuncionalidade.java

--- src/main/java/funcionalidades/BuscandoProdutoFuncionalidade.java
+++ src/main/java/funcionalidades/BuscandoProdutoFuncionalidade.java
@@ -1,58 +1,102 @@
 package funcionalidades;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
-import pageobjects.BuscandoProdutoPage;
-import util.Navegacao;
+import pageobjetcs.PaginaInicial;
+import pageobjetcs.Resultado;
+import util.Navegador;
+import util.Print;
 
-public class BuscandoProdutoFuncionalidade {
-	WebDriver driver;
-	BuscandoProdutoPage buscandoProdutoPage;
-
-	public BuscandoProdutoFuncionalidade() {
-		driver = Navegacao.getChromeDriver();
-		buscandoProdutoPage = new BuscandoProdutoPage(driver);
+public class ResultadoFuncionalidade{
+	private WebDriver driver;
+	private PaginaInicial paginaInicial;
+	private Resultado resultado;
+	private Print print;
+	private Navegador navegador;
+	private Actions action;
+	
+	public ResultadoFuncionalidade(){
+		
+		driver = Navegador.getChromeDriver();
+		paginaInicial = new PaginaInicial(driver);
+		resultado = new Resultado(driver);
+		print = new Print();
+		navegador = new Navegador();
+		action = new Actions(driver);
 	}
+	
+	/**
+	 * Acessa a categoria de ajuda 
+	 */
+	public void acessarAjuda(){
 
-	public void realizaBusca(String produto) {
-		try {
-			buscandoProdutoPage.setCampoBuscar(produto);
-			buscandoProdutoPage.clickBotaoBuscar();
-			driver.wait(3000);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		resultado.clickPerguntasFrequentes();
+		
 	}
-
-	public void imprimindoResultadosGrid(int numeroLinha) {
-		List<WebElement> listaElementos = buscandoProdutoPage.getGridResultadosBusca();
-		int cont = 1;
-		System.out.println("**********Resultados da grid**********\n");
-		for (WebElement webElement : listaElementos) {
-			System.out.println("Titulo: " + buscandoProdutoPage.getTituloProduto(webElement).getText() + "\n");
-
-			WebElement precoProduto = buscandoProdutoPage.getValorProduto(webElement);
-			if (precoProduto != null) {
-				System.out.println("O Valor do produto � : " + precoProduto.getAttribute("innerText") + "\n");
-			}
-			
-			if (cont == numeroLinha) {
-				break;
-			}
-			cont++;
-		}
+	
+	/**
+	 * Realiza a expansão dos elementos de perguntas frequentes, apresentados na tela de "Ajuda"
+	 */
+	public void expandirPerguntas( ){
+		
+		resultado.clickPerguntas();
+		String perguntas = resultado.getTextPerguntas();
+		System.out.println(perguntas);
+		print.tirarPrint(driver, "Ex1 - PerguntasFrequentes");
+		//driver.close();
 	}
-
-	public void realizaPaginacao(String pag) {
-		try {
-			buscandoProdutoPage.clickPaginacao(pag);
-			driver.wait(3000);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());;
-		}
+	
+	/**
+	 * Acessa menu Institucional
+	 */
+	public void acessaInstitucional(){
+		
+		resultado.clickPerguntasFrequentes();
+		resultado.clickInstitucional();
+		
 	}
+	
+	/**
+	 * Valida todas as informações institucionais como Telefone, Atendimento, Onde localizar, Onde comprar cartão e enviar currículo
+	 */
+	public void validaInformacoesIntitucionais(){
+		
+		resultado.getTelefoneCartao();
+		resultado.getAtendimentoLoja();
+		resultado.getOndeLocalizar();
+		resultado.getComprarCartao();
+		resultado.getEnviarCurriculo();
+		print.tirarPrint(driver, "Ex2 - Institucional");
+		//driver.close();
+		
+	}
+	
+	/**
+	 * Acessar menu de telefone do cartão
+	 */
+	public void acessarTelefonedoCartao(){
+		
+		resultado.clickPerguntasFrequentes();
+		resultado.clickInstitucional();
+		resultado.clickTelefoneCartao();
+		
+	}
+	
+	/** Baixa arquivo de imagem dos cartões
+	 * @throws IOException
+	 * @throws MalformedURLException
+	 */
+	public void downloadImg(){
+		
+		action.contextClick(resultado.getImg()).build().perform();
+		action.sendKeys(Keys.SHIFT, "s").build().perform();
+		print.tirarPrint(driver, "Ex3 - ImagemCartão");
+		//driver.close();
+	}	
 
 }
